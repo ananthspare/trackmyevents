1349b2d329e053e695ef8bb03662cc068bcbc8d8
Your Name <you@example.com>
2025-06-19 17:59:12 +0530

    feat: implement tab navigation for categories and calendar; enhance header layout and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
+import { CategoriesComponent } from './categories/categories.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 // Configure Amplify
 Amplify.configure(outputs);
@@ -15,7 +17,9 @@ Amplify.configure(outputs);
     CommonModule,
     RouterOutlet,
     RouterModule,
-    AmplifyAuthenticatorModule
+    AmplifyAuthenticatorModule,
+    CategoriesComponent,
+    CalendarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
@@ -23,6 +27,11 @@ Amplify.configure(outputs);
 export class AppComponent {
   title = 'Event Countdown App';
   currentYear = new Date().getFullYear();
+  activeTab = 'categories';
 
   constructor(public authenticator: AuthenticatorService) {}
+
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
